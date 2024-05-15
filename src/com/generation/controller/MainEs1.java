e713ad3218c46f8937afb937609a073540d86bc8
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-05-15 12:23:23 +0200

    v3

--- src/com/generation/controller/MainEs1.java
+++ src/com/generation/controller/MainEs1.java
@@ -1,9 +1,12 @@
 package com.generation.controller;
 
+import ExceptionHandler.ExceptionLogger;
+
 public class MainEs1 
 {
     public static void main(String[] args) 
     {
+        ExceptionLogger.startLogging();
         System.out.println(a().split(",")[1].equals("6"));    
     }
 
